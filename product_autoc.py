5ec6d95f608ea981d8df8cea87461478220832ca
kenlimitless <ken.lim@emc.com>
2017-10-22 15:06:09 -0700

    Add simple memcache support

--- product_autoc.py
+++ product_autoc.py
@@ -3,6 +3,7 @@ import json
 import webapp2
 
 from google.appengine.ext import ndb
+from google.appengine.api import memcache
 
 class BBProduct(ndb.Model):
     name = ndb.StringProperty(indexed=False)
@@ -13,19 +14,21 @@ class BBProductSearch(webapp2.RequestHandler):
         def to_name(p):
             return p.name
 
-        products = self._search_by_name(self.request.get('prefix'))
-        print("products", products)
+        prefix = self.request.get('prefix')
+        products = memcache.get(self._get_memcache_search_key(prefix)) or self._search_by_name(prefix)
         self.response.headers['Content-Type'] = 'application/json'
         self.response.write(json.dumps(map(to_name, products)))
 
     def _search_by_name(self, prefix):
-        prefix_lower = prefix.lower()
-        prefix_upper = prefix_lower + u'\ufffd'
-        print("prefix_lower", prefix_lower)
-        print("prefix_upper", prefix_upper)
-        products_query = BBProduct.query().filter(BBProduct.name_lower >= prefix_lower).filter(BBProduct.name_lower < prefix_upper)
+        prefix_left = prefix.lower()
+        prefix_right = prefix_left + u'\ufffd'
+        products_query = BBProduct.query().filter(BBProduct.name_lower >= prefix_left).filter(BBProduct.name_lower < prefix_right)
         products = products_query.fetch(10)
+        memcache.set(self._get_memcache_search_key(prefix), products)
         return products
+
+    def _get_memcache_search_key(self, prefix):
+        return 'product:search:'.format(prefix)
 
 app = webapp2.WSGIApplication([
     ('/product/', BBProductSearch)
